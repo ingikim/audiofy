1f0d8d213265eab03ae48cb104c2244b40ac2580
ingikim <gokim365@gmail.com>
2015-08-04 10:48:40 -0700

    Created REST Routes

--- audiofy/models/Posts.js
+++ audiofy/models/Posts.js
@@ -1,4 +1,4 @@
-var mongoose - require('mongoose');
+var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   title: String,
@@ -6,5 +6,10 @@ var PostSchema = new mongoose.Schema({
   upvotes: {type: Number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
+
+PostSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+}
 
 mongoose.model('Post', PostSchema);
